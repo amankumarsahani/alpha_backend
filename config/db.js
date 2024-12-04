2aa9908b88491a736b178865323fc5908f26eb1e
karan7638 <karan2421kumar@gmail.com>
2024-12-04 22:41:03 +0530

    code refactor

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'email_marketing'
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_NAME || 'alpha_d'
 });
 
 module.exports = pool.promise();
